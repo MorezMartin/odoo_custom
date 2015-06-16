85c3204d412037eb4ac13494bcf36674da0b9a02
Martin Morez <morez.martin@gmail.com>
2015-06-16 03:49:08 +0200

    commit

--- mep_serv/mep_serv.py
+++ mep_serv/mep_serv.py
@@ -1,25 +1,35 @@
 from openerp import fields, api
 from openerp.models import Model
 
-#creer automatiquement plusieurs lignes d'enregistrements definies dans code
-#essayer d'abord avec valeurs fixes
-#ensuite essayer avec valeurs heritees de sale.order
+#TODO heriter une classe de sale.order et surcharger par une fonction
+#qui remplacera les lignes (arguments) de mep_serv
+
+#TODO ensuite dans la class mep_serv appeler la fonction de la class
+#sale.order surchargee : comme on pourrait appeler compute_all pour
+#les taxes
 
 
 class mep_serv(Model):
+
     _name = "sale.mep_serv"
 
     name = fields.Char('Order Reference')
     date_order = fields.Datetime('Date')
-    partner_id = fields.Many2one('res.partner', 'Customer')
+    partner_id = fields.Many2one('Customer', compute='_compute')
 #    sale_order_id = fields.Many2one('sale.order', 'Sale Order')
 
-#    def _compute(self):
-#        for so in self :
-#            so.name = "test"
+    def _compute(self):
+        orders = self.env['sale.o'].search([('state', '=', 'done')])
+        for order in orders:
+            self.partner_id = order.make_mep(order)
 
-    @api.model
-    @api.v8
-    @api.returns
-    def create(self):
-        return env[self].create({'name':"test"})
+class sale_o(Model):
+
+    _name = "sale.o"
+    _inherit = "sale.order"
+
+    def make_mep(self, order):
+        mep_vals = {
+                'partner_id': order.partner_id.id
+                }
+        return mep_vals
