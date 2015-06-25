72471a79088fead489799d9ef9218f04f7ae05f2
Morez Martin <morez.martin@gmail.com>
2015-06-25 16:29:54 +0200

    commit

--- mep_serv/mep_serv.py
+++ mep_serv/mep_serv.py
@@ -14,6 +14,23 @@ class sale_o(Model):
     _name = "sale.order"
     _inherit = "sale.order"
 
+    state = fields.Selection([
+            ('draft', 'Draft Quotation'),
+            ('sent', 'Quotation Sent'),
+            ('cancel', 'Cancelled'),
+            ('waiting_date', 'Waiting Schedule'),
+            ('reserved', 'Reserved'),
+            ('progress', 'Sales Order'),
+            ('manual', 'Sale to Invoice'),
+            ('shipping_except', 'Shipping Exception'),
+            ('invoice_except', 'Invoice Exception'),
+            ('done', 'Done'),
+            ], 'Status', readonly=True, copy=False, help="Gives the status of the quotation or sales order.\
+              \nThe exception status is automatically set when a cancel operation occurs \
+              in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
+               but waiting for the scheduler to run on the order date.", select=True)
+
+
     @api.multi
     def create_mep(self):
         mep = self.env['sale.mep_serv']
@@ -37,22 +54,6 @@ class sale_o(Model):
     def action_button_reserv(self):
         self.state = 'reserved'
         return True
-
-    state = fields.Selection([
-            ('draft', 'Draft Quotation'),
-            ('sent', 'Quotation Sent'),
-            ('cancel', 'Cancelled'),
-            ('waiting_date', 'Waiting Schedule'),
-            ('reserved', 'Reserved'),
-            ('progress', 'Sales Order'),
-            ('manual', 'Sale to Invoice'),
-            ('shipping_except', 'Shipping Exception'),
-            ('invoice_except', 'Invoice Exception'),
-            ('done', 'Done'),
-            ], 'Status', readonly=True, copy=False, help="Gives the status of the quotation or sales order.\
-              \nThe exception status is automatically set when a cancel operation occurs \
-              in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
-               but waiting for the scheduler to run on the order date.", select=True)
 
 
 
