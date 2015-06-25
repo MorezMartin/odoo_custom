595ede3006b4d3095cc32e5a5c91e6a6147e3ac0
Morez Martin <morez.martin@gmail.com>
2015-06-25 21:03:14 +0200

    commit

--- traiteur_form/traiteur_form.py
+++ traiteur_form/traiteur_form.py
@@ -15,9 +15,11 @@ class sale_o_l(Model):
     _inherit = 'sale.order.line'
     _name = 'sale.order.line'
 
-    timing = fields.Datetime('Timing', default=_get_date_order)
-
     @api.multi
     def _get_date_order(self):
         res = self.order_id.date_order
         return res
+
+    timing = fields.Datetime('Timing', default=_get_date_order)
+
+
