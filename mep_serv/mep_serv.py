85f0248dcdaaa77e60de3c0a94eccf05191d6ed6
Morez Martin <morez.martin@gmail.com>
2015-06-25 14:05:30 +0200

    commit

--- mep_serv/mep_serv.py
+++ mep_serv/mep_serv.py
@@ -34,7 +34,29 @@ class sale_o(Model):
         self.create_mep()
         return True
 
-#testaaaaaaaa
+    def action_button_reserv(self):
+        self.state = 'reserved'
+        return True
+
+    self.state = fields.Selection([
+            ('draft', 'Draft Quotation'),
+            ('sent', 'Quotation Sent'),
+            ('cancel', 'Cancelled'),
+            ('waiting_date', 'Waiting Schedule'),
+            ('reserved', 'Reserved'),
+            ('progress', 'Sales Order'),
+            ('manual', 'Sale to Invoice'),
+            ('shipping_except', 'Shipping Exception'),
+            ('invoice_except', 'Invoice Exception'),
+            ('done', 'Done'),
+            ], 'Status', readonly=True, copy=False, help="Gives the status of the quotation or sales order.\
+              \nThe exception status is automatically set when a cancel operation occurs \
+              in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
+               but waiting for the scheduler to run on the order date.", select=True)
+
+
+
+
 class sale_mep_serv(Model):
 
     _name = "sale.mep_serv"
