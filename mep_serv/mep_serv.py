2b1beabb02911284ce7c46dadc06d235ed331c72
Morez Martin <morez.martin@gmail.com>
2015-06-30 21:44:35 +0200

    commit

--- mep_serv/mep_serv.py
+++ mep_serv/mep_serv.py
@@ -95,6 +95,6 @@ class sale_mep_serv(Model):
               \nThe exception status is automatically set when a cancel operation occurs \
               in the invoice validation (Invoice Exception) or in the picking list process (Shipping Exception).\nThe 'Waiting Schedule' status is set when the invoice is confirmed\
                but waiting for the scheduler to run on the order date.", select=True)
-   date_order = fields.Datetime("Date")
+    date_order = fields.Datetime("Date")
    type_presta = fields.Char("Type Presta")
    order_line = fields.One2many('sale.order.line', 'order_id', 'Order Lines', readonly=True)
