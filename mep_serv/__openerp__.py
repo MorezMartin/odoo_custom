00846b9b1c5b06944817a5bb207978fea456caf5
Morez Martin <morez.martin@gmail.com>
2015-06-30 11:26:57 +0200

    commit

--- mep_serv/__openerp__.py
+++ mep_serv/__openerp__.py
@@ -35,7 +35,7 @@ Ajout champs formulaire ventes.
     'images' : [],
     'depends' : ["sale", "report"],
     'data':
-    ['mep_serv_view.xml', "report_mep.xml"],
+    ['mep_serv_view.xml', "report_mep.xml", "reportmep.xml"],
     'demo': [],
     'test' :[],
     'installable': True,
