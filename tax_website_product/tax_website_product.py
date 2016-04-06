a925254a03997c1af0191aaed5cebffd4dcb3b3a
Morez Martin <morez.martin@gmail.com>
2016-04-06 17:27:03 +0200

    commit

--- tax_website_product/tax_website_product.py
+++ tax_website_product/tax_website_product.py
@@ -16,6 +16,6 @@ class tax_website_product(Model):
         for product in self:
             cur_obj = self.env["res.currency"]
             tax_obj = self.env["account.tax"]
-            product.tax_price = product.taxes_id._unit_compute(product.taxes_id, product.price, product.id)['amount']
+            product.tax_price = product.taxes_id._unit_compute(product.taxes_id, product.price, product.id)
 #            for taxes in tax_obj:
 #                product.tax_price = product.order_id.pricelist_id.currency_id.round(taxes['amount'] / product.product_uom_qty)
