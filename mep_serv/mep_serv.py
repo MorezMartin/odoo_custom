f74ac3d92bc5e86860e6346a5f7fc4a64b7e4a5e
root <root@archlaptop.home>
2017-01-14 17:23:42 +0100

    commit

--- mep_serv/mep_serv.py
+++ mep_serv/mep_serv.py
@@ -3,7 +3,7 @@ from openerp.models import Model
 import openerp.addons.decimal_precision as dp
 
 class sale_o_config(Model):
-    _name = "sale.order.config"
+    _name = "sale.order.config_s"
 
     name = fields.Char("Nom", required=True)
     config_salle = fields.Text("Description", required=True)
@@ -31,7 +31,7 @@ class sale_o(Model):
 
 
     order_line = fields.One2many('sale.order.line', 'order_id', 'Order Lines', readonly=True, states={'draft': [('readonly', False)], 'reserved': [('readonly', False)], 'sent': [('readonly', False)]}, copy=True)
-    config_s = fields.One2many('sale.order.config', 'config_salle', 'Configuration de salle')
+    config_s = fields.One2many('sale.order.config_s', 'config_salle', 'Configuration de salle')
     horaire_fin = fields.Datetime('Horaire de fin', required=True, default=fields.Datetime.now)
     date_order = fields.Datetime('Date', required=True, readonly=True, select=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)], 'reserved': [('readonly', False)],}, copy=True)
     type_presta = fields.Selection([
