404a8c5d590b938c72fd12a6d229fe51ee26a646
Martin <morez.martin@gmail.com>
2018-04-26 17:37:23 +0200

    key error test to change sense of class

--- subline/subline.py
+++ subline/subline.py
@@ -2,21 +2,6 @@ from openerp import fields, api
 from openerp.models import Model
 import openerp.addons.decimal_precision as dp
 
-class subline(Model):
-    _inherit = 'sale.order.line'
-
-    possibilities = fields.One2many(comodel_name = 'sale.order.line.possibility', inverse_name = 'line_id', string = 'Possibilities', readonly=True, compute='_computeo2m')
-#    options = fields.One2many('sale.order.line.options', 'line_id', "Options", readonly=True)
-
-    @api.v8
-    @api.multi
-    @api.depends("product_id")
-    @api.model
-    def _computeo2m(self):
-        for poss, product in self.possibility, self.product_id.alternative_product_ids:
-            poss.product_id = product
-
-
 def possibilities(Model):
     _name = 'sale.order.line.possibility'
 
@@ -31,5 +16,20 @@ def possibilities(Model):
     def _compute_price(self):
         for poss in self:
             poss.price = 1.0
+
+
+class subline(Model):
+    _inherit = 'sale.order.line'
+
+    possibilities = fields.One2many('sale.order.line.possibility', 'line_id', 'Possibilities', readonly=True, compute='_computeo2m')
+#    options = fields.One2many('sale.order.line.options', 'line_id', "Options", readonly=True)
+
+    @api.v8
+    @api.multi
+    @api.depends("product_id")
+    @api.model
+    def _computeo2m(self):
+        for poss, product in self.possibility, self.product_id.alternative_product_ids:
+            poss.product_id = product
 
 
