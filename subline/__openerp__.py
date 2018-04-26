31aab08491036c317c78875f362de8081f4cee99
Martin <morez.martin@gmail.com>
2018-04-26 15:09:00 +0200

    create subline

---
+++ subline/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Sublines in sale orders for alternative products and options',
+    'version' : '8.0.1.0',
+    'category' : 'Sales',
+    'description' : """
+Add sublines on Sale Order Lines.
+============================================
+""",
+    'author' : 'Martin Morez',
+    'website' : 'http://www.mh-receptions.com',
+    'images' : [],
+    'depends' : ['sale', 'account_voucher', 'account', 'report'], # TODO : Add report
+#    'data': ['tax_line_report.xml'],
+    'demo': [],
+    'test' : [],
+    'installable' : True,
+    'auto_install' : False,
+}
