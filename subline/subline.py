796ed72d002b5b8f2fb4d522000079b66eb7f6f9
Martin <morez.martin@gmail.com>
2018-04-26 15:49:43 +0200

    key error test to change sense of class

--- subline/subline.py
+++ subline/subline.py
@@ -22,7 +22,7 @@ class subline(Model):
     _inherit = 'sale.order.line'
     _name = 'sale.order.line'
 
-    possibilities = fields.One2many('sale.order.line.possibility', 'line_id', readonly=True, """compute='_computeo2m'""")
+    possibilities = fields.One2many('sale.order.line.possibility', 'line_id', readonly=True)#, compute='_computeo2m'
 #    options = fields.One2many('sale.order.line.options', 'line_id', "Options", readonly=True)
 
     @api.v8
