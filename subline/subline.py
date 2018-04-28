992cdea695d4afbed6888b65bff7c85f6e168540
Martin <morez.martin@gmail.com>
2018-04-28 12:26:27 +0200

    commit

--- subline/subline.py
+++ subline/subline.py
@@ -31,6 +31,7 @@ class subline(Model):
     @api.model
     def _computeo2m(self):
         for line in self:
+            line.poss_ids.line_id = line.id
             line.poss_ids.product_id = line.product_id.alternative_product_ids
 #        for poss, product in self.poss_ids, self.product_id.alternative_product_ids:
 #            poss.product_id = product
