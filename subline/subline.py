f2f2380b25eac767a95b6aca63ac7c09c3a8afc2
Martin <morez.martin@gmail.com>
2018-04-28 11:06:26 +0200

    commit

--- subline/subline.py
+++ subline/subline.py
@@ -20,7 +20,7 @@ class sale_order_line_possibility(Model):
 
 class subline(Model):
     _inherit = 'sale.order.line'
-    _name = 'sale.order.bline'
+    _name = 'sale.order.line'
 
     poss_ids = fields.One2many('sale.order.line.possibility', 'line_id', 'Possibilities', compute='_computeo2m')
 #    options = fields.One2many('sale.order.line.options', 'line_id', "Options", readonly=True)
