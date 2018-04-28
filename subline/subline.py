a57616290db12af65d5771221d559adf30d24010
Martin <morez.martin@gmail.com>
2018-04-28 15:43:53 +0200

    commit

--- subline/subline.py
+++ subline/subline.py
@@ -6,7 +6,7 @@ class sale_order_line_possibility(Model):
     _name = 'sale.order.line.possibility'
 
     line_id = fields.Many2one('sale.order.line')
-    product_id = fields.Many2one("product.product", "Product", domain=[('sale_ok', '=', True)], readonly=True)
+    product_id = fields.Many2one("product.product", "Product", domain=[('sale_ok', '=', True)], readonly=True, compute='_compute_o2m')
     price = fields.Float(compute='_compute_price')
 
     @api.v8
@@ -14,27 +14,34 @@ class sale_order_line_possibility(Model):
 #    @api.depends("product_id")
     @api.model
     def _compute_price(self):
-        for poss in self:
-            poss.price = 1.0
+        self.price = 1.0
+
+    @api.v8
+    @api.multi
+#    @api.depends("product_id")
+    @api.model
+    def _compute_o2m(self):
+        products = line_id. 
 
 
 class subline(Model):
     _inherit = 'sale.order.line'
     _name = 'sale.order.line'
 
+    alt = self.product_id.alternative_product_ids
     poss_ids = fields.One2many('sale.order.line.possibility', 'line_id', 'Possibilities')#, compute='_computeo2m')
 #    options = fields.One2many('sale.order.line.options', 'line_id', "Options", readonly=True)
 
-    @api.v8
-    @api.multi
-    @api.depends("id", "product_id", "product_id.alrternative_product_ids")
-    @api.model
-    def _computeo2m(self):
-        for line in self:
-            for poss in line.poss_ids:
-                poss.line_id = line.id
-                for product in line.product_id.alternative_product_ids:
-                    poss.product_id = product.id
+#    @api.v8
+#    @api.multi
+#    @api.depends("id", "product_id", "product_id.alternative_product_ids")
+#    @api.model
+#    def _computeo2m(self):
+#        for line in self:
+#            for poss in line.poss_ids:
+#                poss.line_id = line.id
+#                for product in line.product_id.alternative_product_ids:
+#                    poss.product_id = product.id
 #        for poss, product in self.poss_ids, self.product_id.alternative_product_ids:
 #            poss.product_id = product
 
