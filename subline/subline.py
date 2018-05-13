a6ed8a91b1e1b1e5b7433cab7fa2e3a815587e79
Martin <morez.martin@gmail.com>
2018-05-13 02:09:39 +0200

    commit

--- subline/subline.py
+++ subline/subline.py
@@ -25,6 +25,6 @@ class subline(Model):
         product_ids = self.product_id.alternative_product_ids
         poss = self.env['sale.order.line.possibility']
         for prod in product_ids:
-            vals = {'line_id': self, 'product_id': prod}
+            vals = {'line_id': record.id, 'product_id': prod.id}
             poss.create(vals)
         return record
