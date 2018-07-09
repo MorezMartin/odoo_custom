0899bda0036f577d534eee244fd684a953d16870
Martin <morez.martin@gmail.com>
2018-07-09 20:56:10 +0200

    commit

--- subline/subline.py
+++ subline/subline.py
@@ -70,13 +70,13 @@ class subline(Model):
         poss = self.env['sale.order.line.possibility']
         opt = self.env['sale.order.line.option']
         if [poss.product_id for poss in self.poss_ids] != [prod for prod in product_poss_ids]:
-            self.poss_ids.unlink()
+#            self.poss_ids.unlink()
             poss.unlink()
             for prod in product_poss_ids:
                 vals = {'line_id': self.id, 'product_id': prod.id}
                 poss.create(vals)
         if [opt.product_id for opt in self.opt_ids] != [prod2 for prod2 in product_opt_ids]:
-            self.opt_ids.unlink()
+#            self.opt_ids.unlink()
             opt.unlink()
             for opt in product_opt_ids:
                 vals = {'line_id': self.id, 'product_id': prod2.id}
